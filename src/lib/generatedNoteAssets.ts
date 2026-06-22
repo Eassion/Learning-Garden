631ac7c277ecacc6184ee94a0f1aae0e7095b098
Eassion <2573221732@xiaomi.com>
2026-06-22 09:54:39 +0800

    docs: 新增2026-6-22笔记

--- src/lib/generatedNoteAssets.ts
+++ src/lib/generatedNoteAssets.ts
@@ -4,5 +4,8 @@ export const generatedNoteAssets: Record<string, PostAssetMap> = {
   "../content/posts/openSpec-superpowers-gstack.md": {
     "file:///C:/Users/23738/AppData/Roaming/marktext/images/2026-06-12-16-44-46-image.png": "/note-assets/openSpec-superpowers-gstack/2026-06-12-16-44-46-image-62ba79e4.png",
     "file:///C:/Users/23738/AppData/Roaming/marktext/images/2026-06-12-18-07-02-image.png": "/note-assets/openSpec-superpowers-gstack/2026-06-12-18-07-02-image-2e58d7fe.png"
+  },
+  "../content/posts/怎么让Agent处理大文件.md": {
+    "file:///C:/Users/23738/AppData/Roaming/marktext/images/2026-06-22-09-41-36-5315ba7e11f0330a41ca3a53e0a521d2.png": "/note-assets/怎么让Agent处理大文件/2026-06-22-09-41-36-5315ba7e11f0330a41ca3a53e0a521d2-c0bb563e.png"
   }
 };
